bd5ac5e9224f6ad091d9d32c62206ce2428c9225
olgacherry <zebroid81@yandex.ru>
2023-02-14 20:33:28 +0300

    задача билета № 7

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static int square(int a){
+        int result = a*a;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int a =square(8);
+
+        System.out.println(a);
+    }
+
+}
